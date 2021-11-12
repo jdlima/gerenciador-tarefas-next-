d6768ab1109de4e3268f8cc0396e85fc46793794
jdlima <joao.diego.r.lima@gmail.com>
2021-11-12 20:05:43 -0300

    Criar Projeto e API inicial, Integração com banco de dados, API de Usuário, JWT e API de Tarefas

---
+++ models/TaskModel.ts
@@ -0,0 +1,12 @@
+import mongoose, {Schema} from 'mongoose';
+
+const TaskSchema = new Schema({
+ name: { type : String, required : true},
+ userId: { type : String, required : true},
+ finishPrevisionDate: { type : Date, required : true},
+ finishDate: { type : Date, required : false},
+
+});
+
+
+export const TaskModel = mongoose.models.tasks || mongoose.model('tasks', TaskSchema);
---
+++ types/Tasks.ts
@@ -0,0 +1,8 @@
+
+export type Task = {
+    _id?: string
+    userId : string
+    name : string
+    finishPrevisionDate: Date
+    finishDate?: Date
+    }
